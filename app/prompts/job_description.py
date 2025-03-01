b207a50198dafdc020c713b267eb4d5d5b2ed780
daianeklein <daiane.klein22@gmail.com>
2025-03-01 14:46:56 -0300

    add prompt

---
+++ app/prompts/job_description.py
@@ -0,0 +1,29 @@
+job_description_text = '''
+About the job
+O(a) Analista de Dados tem como principal responsabilidade projetar, construir e otimizar pipelines de dados em um ambiente Azure. Irá garantir a integridade, segurança e eficiência dos dados, suportando a estratégia de negócio da organização. Além disso, irá colaborar na implementação de práticas de Governança de Dados, assegurando que os dados estejam alinhados às necessidades da empresa, aos requisitos de segurança e às regulamentações legais. Esse papel é vital para o desenvolvimento de soluções modernas e inovadoras, permitindo o uso eficiente de grandes volumes de dados em toda a organização. Modelo de trabalho 100% remoto. O escritório é localizado em Porto Alegre.
+
+A Agrofel está ao lado do produtor rural, oferecendo soluções completas que vão do plantio à colheita, incluindo insumos, recebimento e comercialização de grãos. Recentemente, expandimos nossos serviços com a Agrofel Soluções Financeiras criando uma plataforma integrada que proporciona mais prosperidade e crescimento ao produtor ao longo de todo o ciclo produtivo.
+
+Responsabilidades e atribuições
+
+Criar e manter pipelines de dados eficientes e escaláveis para suportar processos analíticos e operacionais;
+Realizar processos de ETL/ELT, garantindo a consistência, qualidade e segurança dos dados;
+Auxiliar na manutenção de repositórios de dados, como data lakes e data warehouses, no ambiente Azure;
+Implementar práticas de governança de dados para assegurar a qualidade, segurança e compliance das informações;
+Identificar e corrigir inconsistências e anomalias nos dados, propondo melhorias contínuas nos processos;
+Colaborar com diferentes equipes para alinhar as soluções de dados às demandas do negócio;
+Apoiar a disseminação da cultura orientada a dados na organização, explicando processos de ingestão e manipulação de dados;
+Elaborar e manter documentações claras sobre os processos e soluções de dados.
+
+
+Requisitos e qualificações
+
+Graduação em áreas como Ciência de Dados, Tecnologia da Informação, Engenharia de Computação ou similares;
+Experiência com linguagens de manipulação de dados, como SQL (T-SQL) e Python;
+Experiência com ferramentas de visualização de dados, como Power BI, incluindo criação de métricas(DAX) e dashboards;
+Experiência com alguma ferramenta de ingestão e integração de dados da Microsoft, como Azure Data Factory, Azure Synapse Analytics, Databricks e Microsoft Fabric;
+Conhecimento em modelagem de dados e arquiteturas de Data Lake e Data Warehouse;
+Noções de PySpark para manipulação de dados em grande escala;
+Experiência em integração de dados via APIs REST.
+
+'''
